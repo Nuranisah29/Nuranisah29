4e9e44c0ae3864c8f889292934df20d2c17006f3
Nuranisah29 <138289383+Nuranisah29@users.noreply.github.com>
2023-07-08 13:41:36 +0700

    mengubah body pelanggaran

--- app/pelanggan/form_pelanggan.php
+++ app/pelanggan/form_pelanggan.php
@@ -7,7 +7,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <tittle>Form Apotek</tittle>
+        <tittle>Form Pelanggan</tittle>
     </head>
 
     <body>
@@ -17,8 +17,8 @@
             <input type="text" name="nama" id="nama">
         </div>
         <div>
-            <label for="jenis_kelamin">Jenis Kelamin</label>
-            <input type="text" name="jenis_kelamin" id="jenis_kelamin">
+            <label for="Alamat">Alamat</label>
+            <input type="text" name="alamat" id="alamat">
         </div>
         
         <div>
--- app/pelanggan/list_pelanggan.php
+++ app/pelanggan/list_pelanggan.php
@@ -6,19 +6,19 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <tittle>Apotek</tittle>
+        <tittle>Pelanggan</tittle>
     </head>
 </html>
 <body>
-    <a href="form_apotek.php">Tambah Data</a>
+    <a href="form_pelanggan.php">Tambah Data</a>
     <?php
     ?>
     <table>
         <thead>
         <tr>
             <th>Nama</th>
-            <th>Jenis Kelamin</th>
             <th>Umur</th>
+            <th>Alamat</th>
             <th>Id_Pelanggan</th>
             <th>Act</th>
         </tr>
